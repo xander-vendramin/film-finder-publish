5a84811a3476f70921e2a05b064b1ce11e215c9d
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-01-28 13:04:21 -0600

    Delete film-finder/src/app/Components directory

--- film-finder/src/app/Components/logo.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-export default function Logo() {
-  return (
-    <div>
-      <Image src="/Film_Finder.svg" width={100} height={100} alt="Brand Logo" />
-    </div>
-  );
-}
