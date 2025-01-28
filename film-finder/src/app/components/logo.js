7b020e84cd7451c386ff8eb460ab41216e542adc
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-01-28 13:02:17 -0600

    Add files via upload

---
+++ film-finder/src/app/components/logo.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function Logo() {
+  return (
+    <div>
+      <Image src="/Film_Finder.svg" width={100} height={100} alt="Brand Logo" />
+    </div>
+  );
+}
